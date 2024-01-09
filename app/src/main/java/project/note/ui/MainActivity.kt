0e85054f9997b6a69e0330064b77e2d618d0a403
Ihar <ihar.fedarau@gmail.com>
2024-01-09 13:38:55 +0300

    Splashscreen time appearance depends on time of loading of data

--- app/src/main/java/project/note/ui/MainActivity.kt
+++ app/src/main/java/project/note/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package project.note.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,16 +13,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import project.note.R
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import project.note.database.Note
+import project.note.databinding.MainActivityLayoutBinding
+import project.note.viewmodels.NoteViewModel
 
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
     private var keepSplashScreen = true
+    private lateinit var binding: MainActivityLayoutBinding
+    private lateinit var adapter: ScreenSlidePagerAdapter
+    private val noteViewModel: NoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,33 +40,91 @@ class MainActivity : FragmentActivity() {
                 }
             }
 
-        lifecycleScope.launch {
-            delay(500)
-            keepSplashScreen = false
-        }
-
         setContent {
             MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Content()
+                    Content(this)
                 }
             }
         }
     }
+
+    fun initialize(context: Context): View {
+        binding = MainActivityLayoutBinding.inflate(LayoutInflater.from(context), null, false)
+
+        adapter = ScreenSlidePagerAdapter(emptyList(), this)
+        binding.pager.adapter = adapter
+
+        noteViewModel.allNotes.observe(this) { list ->
+            keepSplashScreen = false
+
+            // Insertion
+            if (adapter.itemCount > 0 && adapter.itemCount < list.size) {
+                adapter.updateItems(list)
+                binding.pager.currentItem = list.size - 1
+            } else {
+                adapter.updateItems(list)
+            }
+        }
+
+        binding.insert.setOnClickListener {
+            noteViewModel.insert(Note("", ""))
+        }
+
+        supportFragmentManager.setFragmentResultListener(
+            "deleteNoteRequestKey",
+            this
+        ) { _, bundle ->
+            noteViewModel.delete(bundle.getInt("bundleDeleteNoteKey"))
+        }
+
+        supportFragmentManager.setFragmentResultListener(
+            "saveNoteRequestKey",
+            this
+        ) { _, bundle ->
+            bundle.getString("bundleSaveNoteKey")?.let {
+                val note = Json.decodeFromString<Note>(it)
+                noteViewModel.update(note)
+            }
+        }
+
+        return binding.root
+    }
+
+    inner class ScreenSlidePagerAdapter(private var notes: List<Note>, fa: FragmentActivity) :
+        FragmentStateAdapter(fa) {
+        override fun getItemId(position: Int): Long {
+            return hash(notes[position])
+        }
+
+        override fun containsItem(itemId: Long): Boolean {
+            return notes.any { note ->
+                itemId == hash(note)
+            }
+        }
+
+        override fun getItemCount(): Int = notes.size
+
+        override fun createFragment(position: Int): Fragment =
+            NoteFragment(notes[position])
+
+        fun updateItems(items: List<Note>) {
+            notes = items
+            notifyDataSetChanged()
+        }
+
+        private fun hash(note: Note) = note.hashCode().toLong()
+    }
 }
 
 @Composable
-fun Content() {
+fun Content(activity: MainActivity) {
     AndroidView(
         factory = { context ->
-            val view =
-                LayoutInflater.from(context).inflate(R.layout.main_activity_layout, null, false)
-
-            // do whatever you want...
-            view // return the view
+            activity.initialize(context)
         },
         update = {
         }
