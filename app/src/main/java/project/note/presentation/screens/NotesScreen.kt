caf2c213391df6a76b54af54c9dc8f993b6774d5
Ihar <ihar.fedarau@gmail.com>
2024-04-05 16:39:56 +0300

    remove progress indicator during removal

--- app/src/main/java/project/note/presentation/screens/NotesScreen.kt
+++ app/src/main/java/project/note/presentation/screens/NotesScreen.kt
@@ -1,27 +1,19 @@
 package project.note.presentation.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -38,9 +30,6 @@ import project.note.presentation.screens.controls.FloatingActionButton
 fun NotesScreen(onItemClick: (note: Note) -> Unit,
               viewModel: NotesViewModel = hiltViewModel()) {
     val scaffoldState = rememberScaffoldState()
-    var inProgress by rememberSaveable {
-        mutableStateOf(false)
-    }
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -49,10 +38,8 @@ fun NotesScreen(onItemClick: (note: Note) -> Unit,
         },
         floatingActionButton = {
             FloatingActionButton {
-                inProgress = true
                 viewModel.insert(Note("Unknown", "")) {
                     onItemClick(it)
-                    inProgress = false
                 }
             }
         },
@@ -71,8 +58,7 @@ fun NotesScreen(onItemClick: (note: Note) -> Unit,
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(paddings)
-                .padding(24.dp),
+                .padding(paddings),
             verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
             itemsIndexed(items = notes,
@@ -86,14 +72,6 @@ fun NotesScreen(onItemClick: (note: Note) -> Unit,
                         })
                     Divider(color = Color.Black, thickness = 1.dp)
                 })
-        }
-
-        if (inProgress) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center) {
-                CircularProgressIndicator(modifier = Modifier.width(64.dp))
-            }
         }
     }
 }
