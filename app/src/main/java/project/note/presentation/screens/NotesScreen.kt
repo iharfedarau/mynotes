5e9a6fae899b244fb01c98618ff1a71ea7396343
Ihar <ihar.fedarau@gmail.com>
2024-04-05 18:56:27 +0300

    remove unused import

--- app/src/main/java/project/note/presentation/screens/NotesScreen.kt
+++ app/src/main/java/project/note/presentation/screens/NotesScreen.kt
@@ -1,7 +1,6 @@
 package project.note.presentation.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
