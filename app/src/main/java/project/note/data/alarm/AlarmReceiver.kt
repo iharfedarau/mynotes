e5de35adbd04521d097dc49e32dc87939ddeeb1f
Ihar <ihar.fedarau@gmail.com>
2024-04-17 17:44:21 +0300

    Open application from notification

--- app/src/main/java/project/note/data/alarm/AlarmReceiver.kt
+++ app/src/main/java/project/note/data/alarm/AlarmReceiver.kt
@@ -1,25 +1,35 @@
 package project.note.data.alarm
 
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import project.note.NoteApplication
 import project.note.R
+import project.note.presentation.MainActivity
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val message = intent?.getStringExtra("EXTRA_MESSAGE") ?: return
         context?.let { ctx ->
-            val notificationManager =
-                ctx.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val pendingIntent = TaskStackBuilder.create(ctx).run {
+                addNextIntentWithParentStack(Intent(ctx, MainActivity::class.java))
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+
+            val notificationManager = ctx.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             val builder = NotificationCompat.Builder(ctx, NoteApplication.NOTE_CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Note Alarm")
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-            notificationManager.notify(1, builder.build())
+                .setContentIntent(pendingIntent)
+            val notification = builder.build()
+
+            notificationManager.notify(1, notification)
         }
     }
 }
