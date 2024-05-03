a466109f42465cf59fa1bbcb84ed95f8e6877f29
Ihar <ihar.fedarau@gmail.com>
2024-05-03 17:05:03 +0300

    Refactoring

--- app/src/main/java/project/note/presentation/editnote/CustomBottomSheetAction.kt
+++ app/src/main/java/project/note/presentation/editnote/CustomBottomSheetAction.kt
@@ -1,7 +1,7 @@
 package project.note.presentation.editnote
 
-sealed class CustomBottomSheetAction {
-    data object Delete: CustomBottomSheetAction()
-    data object SetAlarm: CustomBottomSheetAction()
-    data object Dismiss: CustomBottomSheetAction()
+sealed interface CustomBottomSheetAction {
+    data object Delete: CustomBottomSheetAction
+    data object SetAlarm: CustomBottomSheetAction
+    data object Dismiss: CustomBottomSheetAction
 }
